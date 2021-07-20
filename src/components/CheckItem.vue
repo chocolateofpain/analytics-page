e550a0f809d22a4db1fef2ed5c5df8885252db04
Elisabeth Vonesch <lisa.vonesch@gmail.com>
2021-07-20 22:36:00 +0200

    expand check row on click

--- src/components/CheckItem.vue
+++ src/components/CheckItem.vue
@@ -1,12 +1,22 @@
 <template>
-  <tr>
-    <td><Icon name="chevron-right" :width="12" /><span class="type">{{ checkItemData.type }}</span></td>
+  <table class="table-check-item">
+  <tr v-on:click="onClickButton" class="check-data-row">
+    <td class="type-col">
+      <Icon :name="showGraph ? 'chevron-down' : 'chevron-right'" :width="12" class="" />
+      <span class="type">{{ checkItemData.type }}</span>
+    </td>
     <td>{{ checkItemData.name }}</td>
-    <td class="align-center">{{ Math.floor(checkItemData.success * 100) }}%</td>
-    <td class="align-center">{{ Math.round(checkItemData.avg) }}ms</td>
-    <td class="align-center">{{ Math.round(checkItemData.p95) }}ms</td>
-    <td class="align-center">{{ Math.round(checkItemData.p99) }}ms</td>
+    <td class="data-cols align-center">{{ Math.floor(checkItemData.success * 100) }}%</td>
+    <td class="data-cols align-center">{{ Math.round(checkItemData.avg) }}ms</td>
+    <td class="data-cols align-center">{{ Math.round(checkItemData.p95) }}ms</td>
+    <td class="data-cols align-center">{{ Math.round(checkItemData.p99) }}ms</td>
   </tr>
+  <tr v-show="showGraph">
+    <td colspan="6">
+      <div>This is a graph</div>
+    </td>
+  </tr>
+  </table>
 </template>
 
 <script>
@@ -14,24 +24,50 @@ import '@primer/octicons/build/build.css'
 import Icon from './library/icon/Icon.vue'
 
 export default {
-  name: 'checkItem',
+  name: 'CheckItem',
   components: {
     Icon,
-  }, 
+  },
   props: [
-    "checkItemData"
-  ]
+    "checkItemData", 
+  ],
+  data () {
+    return {
+      showGraph: true
+    }
+  },
+  methods: {
+    onClickButton () {
+      this.showGraph = !this.showGraph
+    }
+  }
 }
 </script>
 
 <style scoped>
+.expand-button {
+  background: none;
+  height: 38px;
+  width: 38px;
+}
 .type {
   margin-left: 4px;
   vertical-align: middle;
-
 }
-tr {
-  border-bottom: 1px solid #D2D2D2;
+.check-data-row {
   height: 38px;
+}
+.table-check-item {
+  border-bottom: 1px solid #D2D2D2;
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 100%;
+}
+.type-col {
+  width: 100px;
+}
+.data-cols {
+  text-align: center;
+  width: 65px;
 }
 </style>
--- src/components/CheckList.vue
+++ src/components/CheckList.vue
@@ -1,11 +1,16 @@
 <template>
   <tbody>
-    <CheckItem v-for="(item, index) in checks" :key='index' v-bind:checkItemData="item" />
+    <td colspan="6">
+    <CheckItem 
+      @clicked="onClickExpandButton"
+      v-for="(item, index) in checks" :key='index' v-bind:checkItemData="item"
+      v-bind:showGraph="showGraph"
+    />
+    </td>
   </tbody>
 </template>
 
 <script>
-import '@primer/octicons/build/build.css'
 import CheckItem from './CheckItem.vue'
 
 export default {
@@ -13,8 +18,18 @@ export default {
   components: {
     CheckItem,
   }, 
+  data () {
+    return {
+      showGraph: false
+    }
+  },
+  methods: {
+    onClickExpandButton (value) {
+      this.showGraph = value
+    }
+  },
   props: [
     "checks"
-  ]
+  ],
 }
 </script>
