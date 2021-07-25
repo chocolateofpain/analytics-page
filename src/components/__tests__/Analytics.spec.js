cff9c6f75b3ad4c7bbe12d05a84f900e466c917a
Elisabeth Vonesch <lisa.vonesch@gmail.com>
2021-07-25 20:17:03 +0200

    update tests to ensure api call is made

--- src/components/__tests__/Analytics.spec.js
+++ src/components/__tests__/Analytics.spec.js
@@ -1,15 +1,70 @@
 import { mount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 import Analytics from '../Analytics.vue'
 
 describe('Analytics', () => {
-  it('should be defined', () => {
+  let component 
+  
+  beforeAll(() => {
+    jest.mock('axios', () => ({
+      _esmodule: true, 
+      default: {
+        get: jest.fn(() => Promise.resolve({ data: mockChecks }))
+      }
+    }))
+  })
+
+  it('should be defined', async () => {
     expect(Analytics).toBeDefined()
   })
 
   describe('snapshots', () => {
-    it('should render component', () => {
-      const component = mount(Analytics)
+    it('should render component when checks === null', async () => {
+      component = mount(Analytics)
+      await flushPromises()
+      expect(component).toMatchSnapshot()
+    })
+
+    it('should render component when checks !== null', async () => {
+      component = mount(Analytics, {
+        data() {
+          return {
+            checks: mockChecks
+          }
+        }
+      })
+      await flushPromises()
       expect(component).toMatchSnapshot()
     })
   })
+
+  describe('fetchChecks', () => {
+    it('should call fetchChecks on created', async () => {
+      const fetchChecksSpy = jest.spyOn(Analytics.methods, 'fetchChecks')
+      component = mount(Analytics)
+      await flushPromises()
+      expect(fetchChecksSpy).toHaveBeenCalled()
+    })
+  })
 })
+
+const mockChecks = [
+  {
+    "id": "d2a67163-3cfb-476a-aaff-0d0e8ca18a0f",
+    "name": "Check 1",
+    "type": "BROWSER",
+    "success": 0.4989,
+    "avg": 498.558,
+    "p95": 948,
+    "p99": 989
+  },
+  {
+    "id": "5324e4bd-d3a3-490a-b921-d1ed265502d6",
+    "name": "Check 2",
+    "type": "API",
+    "success": 0.49833333333333335,
+    "avg": 496.9130666666667,
+    "p95": 949,
+    "p99": 989
+  }
+]
--- src/components/__tests__/CheckList.spec.js
+++ src/components/__tests__/CheckList.spec.js
@@ -1,15 +1,50 @@
 import { mount } from '@vue/test-utils'
 import CheckList from '../CheckList.vue'
 
-describe('CheckList', () => {
+describe.only('CheckList', () => {
   it('should be defined', () => {
     expect(CheckList).toBeDefined()
   })
 
   describe('snapshots', () => {
-    it('should render component', () => {
+    it('should render component if no data is available', () => {
       const component = mount(CheckList)
+      expect(component).toMatchSnapshot()
+    })
+
+    it('should render component if data is available', () => {
+      const component = mount(CheckList, { dataProps: { checks }})
       expect(component).toMatchSnapshot()
     })
   })
 })
+
+const checks = [
+  {
+    "id": "f676e8c7-a6e4-442a-b5de-27cb213161da",
+    "name": "Check 0",
+    "type": "BROWSER",
+    "success": 0.5039,
+    "avg": 498.7613666666667,
+    "p95": 948,
+    "p99": 990
+  },
+  {
+    "id": "d2a67163-3cfb-476a-aaff-0d0e8ca18a0f",
+    "name": "Check 1",
+    "type": "BROWSER",
+    "success": 0.4989,
+    "avg": 498.558,
+    "p95": 948,
+    "p99": 989
+  },
+  {
+    "id": "5324e4bd-d3a3-490a-b921-d1ed265502d6",
+    "name": "Check 2",
+    "type": "API",
+    "success": 0.49833333333333335,
+    "avg": 496.9130666666667,
+    "p95": 949,
+    "p99": 989
+  }
+]
