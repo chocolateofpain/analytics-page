22993a9f7f0f4b40a8c486582dcfbfd46c7e1858
Elisabeth Vonesch <lisa.vonesch@gmail.com>
2021-07-29 21:40:26 +0200

    test input and clear input method

--- src/components/library/searchbar/__tests__/Searchbar.spec.js
+++ src/components/library/searchbar/__tests__/Searchbar.spec.js
@@ -8,13 +8,43 @@ describe('Searchbar', () => {
 
   describe('snapshots', () => {
     it('should render default component', () => {
-      const component = _mount(Searchbar)
+      const component = _mount({})
       expect(component).toMatchSnapshot()
     })
 
     it('should render with a custom width', () => {
       const component = _mount({ minWidth: 300 })
       expect(component).toMatchSnapshot()
+    })
+
+    it('should render with a custom placeholder', () => {
+      const component = _mount({ placeholder: 'I\'m a random placeholder' })
+      expect(component).toMatchSnapshot()
+    })
+  })
+
+  describe('input functionality', () => {
+    it('should update input', async () => {
+      const component = _mount({})
+      const searchbarInput = component.find('input[type="text"]')
+      await searchbarInput.setValue('hello darkness my old friend')
+      expect(component.find('input[type="text"]').element.value).toBe('hello darkness my old friend')
+    })
+
+    it('should call onClickClearInput', async () => {
+      // TODO: improve test to not throw deprecated warning
+      const onClickClearInputMethod = jest.spyOn(Searchbar.methods, 'onClickClearInput')
+      const component = mount(Searchbar, {
+        attrs: {
+          value: ''
+        },
+        methods: onClickClearInputMethod
+      })
+      const searchbarInput = component.find('input[type="text"]')
+      await searchbarInput.setValue('hello darkness my old friend')
+      const clearInputButton = component.find('.clear-input')
+      clearInputButton.trigger('mousedown', { left: true })
+      expect(onClickClearInputMethod).toHaveBeenCalled()
     })
   })
 
@@ -23,7 +53,10 @@ describe('Searchbar', () => {
    */
   const _mount = (props) => {
     return mount(Searchbar, {
-      propsData: props
+      propsData: props,
+      attrs: {
+        value: ''
+      }
     })
   }
 })
